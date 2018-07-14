2027264b05c6cecdba7951aa6eb6aa926bc49c5c
Will Ley <wley3337@gmail.com>
2018-07-14 18:26:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function calculatesFarePrice(start, destination){
   switch (true) {
     case distance <= 400: 
       return 0;
-    case 400 < distance <= 2000:
+    case 400 < distance < 2000:
       return (distance-400)*2/100; 
     case 2000 < distance < 2500: 
       return  25;
