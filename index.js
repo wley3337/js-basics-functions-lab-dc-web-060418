8cf8068ce6665db0b7885e2459234788d7009404
Will Ley <wley3337@gmail.com>
2018-07-14 18:12:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+
+
+function distanceFromHqInBlocks(block){
+  return Math.abs(block- 42);
+}
+
+function distanceFromHqInFeet(blocks){
+  return( distanceFromHqInBlocks(blocks)* 264);
+}
+
+function distanceTravelledInFeet(block1, block2){
+  return( Math.abs(block1-block2)*264)
+}
+
+function calculatesFarePrice(block1, block2){
+  let distance = distanceTravelledInFeet(block1, block2)
+  
+  switch(distance){
+    case distance <= 400 
+      return "free sample"
+    case 400 < distance <= 2000
+      return (distance-400)*2 
+  400-2000 feet 2cents perfoot not including first 400 
+  over 2000 and under 2500  $25
+  
+  2500 and over return 'cannot travel that far'
+  }
+}
